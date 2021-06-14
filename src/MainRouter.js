f65966fce98dd52a732c6349fab36eb92d0f0fe0
sedx876 <swatkins876@gmail.com>
2021-06-14 07:22:45 -0400

    Added navbar and footer

---
+++ src/MainRouter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Navbar from './core/Navbar'
+import Footer from './core/Footer'
+
+const MainRouter = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Switch>
+
+      </Switch>
+      <Footer/>
+    </div>
+  )
+}
+
+export default MainRouter
---
+++ src/core/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/footer.css'
+
+const Footer = () => {
+  return (
+    <div class="footer gradient">
+      This will always appear at the bottom of the page, but 
+      <strong>not fixed</strong>.
+    </div>
+  )
+}
+
+export default Footer
